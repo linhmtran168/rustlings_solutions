cb6b8cc91d11d0af28e81d3b970e5dc73cd10bd4
Linh Tran <linh.tm@raksul.com>
2022-10-05 16:19:36 +0700

    feat: add solutions

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,21 +1,38 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
-    // TODO: Remove whitespace from the end of a string!
-    ???
+    let mut start: usize = 0;
+    let mut end: usize = 0;
+    let mut started: bool = false;
+
+    for (i, c) in input.chars().enumerate() {
+        if c == ' ' {
+            continue
+        }
+
+        if !started {
+            started = true;
+            start = i;
+        }
+        end = i;
+    }
+
+    if started {
+        input[start..end+1].to_string()
+    } else {
+        String::new()
+    }
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    format!("{} world!", input)
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.replace("cars", "balloons")
 }
 
 #[cfg(test)]
@@ -27,6 +44,8 @@ mod tests {
         assert_eq!(trim_me("Hello!     "), "Hello!");
         assert_eq!(trim_me("  What's up!"), "What's up!");
         assert_eq!(trim_me("   Hola!  "), "Hola!");
+        assert_eq!(trim_me("     "), "");
+        assert_eq!(trim_me(""), "");
     }
 
     #[test]
